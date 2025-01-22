0b67735ed8c3128db3ca2c7b8f3522506a43e0c8
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2025-01-22 23:30:08 +0530

    initial commit

---
+++ MigrateCLI/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MigrateCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
